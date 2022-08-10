498e89963616600e468a9c6ad1ca65b7bd9e1c27
Nikita Davydov <davidovnv@gmial.com>
2022-08-10 21:26:33 +0300

    [design] add color around button

--- design.py
+++ design.py
@@ -2,33 +2,50 @@ import tkinter as tk
 #%%
 class Colors:
     
-    bg = "midnight blue"
+    bg = "navy"
     fg = "lime"
     activebackground = "#38418A",
     activeforeground="forest green"
 
-class Button(Colors, tk.Button):
+
+class Button(tk.Button, Colors):
     
-    def __init__(self, *args, **kwargs):
+    relief="solid"    
+    
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(bg=self.bg, fg=self.fg,
                          activebackground=self.activebackground,
                          activeforeground=self.activeforeground,
-                         *args, **kwargs)
+                         relief=self.relief, borderwidth=3, *args, **kwargs)
 
-class Label(Colors, tk.Label):
+
+class Label(tk.Label, Colors):
     
     bg = "sky blue"
     fg = "black"
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(bg=self.bg, fg=self.fg, *args, **kwargs)
-        
+
+
+class LabelFrame(tk.LabelFrame):
     
+    bg = "midnight blue"
+    bd = 4
     
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(bg=self.bg, bd=self.bd, *args, **kwargs)    
+
+    # def __init__(self, *args, **kwargs) -> None:
+    #     super().__init__(bd = 6, bg = "black", *args, **kwargs)    
+
+
 if __name__ == "__main__":
    
     root = tk.Tk()
-    Button(root, text="Button").pack()
-    Label(root, text="Label").pack()
-    root.mainloop()
+    border = LabelFrame(root)
+    border.pack()
     
+    Button(border, text="Button", width=8).pack()
+    Label(border, text="Label").pack(fill="x")
+    root.mainloop()
