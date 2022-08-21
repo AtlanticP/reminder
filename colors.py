8fbed29e66cef96ac7b232d0f430090eb72069f0
Nikita Davydov <davidovnv@gmial.com>
2022-08-21 15:21:25 +0300

    [color] add colors.py

---
+++ colors.py
@@ -0,0 +1,13 @@
+COLORS: dict[str, dict[str, str]] = {
+        "deep blue": {
+            "bg_main": "#011127",
+            "bg_labelTime": "#062656",
+            "fg_labelTime": "#4FC500",
+            "bg_button": "#062656",
+            "fg_button": "#4FC500",
+            "bg_label": "#89EBEB",
+            "fg_label": "#000000",
+            "actbg_button": "#38418A",
+            "actfg_button": "#4FC500"
+        }
+}
