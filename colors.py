7fcb3852db6f7179b0a74a07e5072742a53bda94
Nikita Davydov <davidovnv@gmial.com>
2022-08-21 16:37:59 +0300

    [color] modify colorsheme to support unpacking

--- colors.py
+++ colors.py
@@ -1,13 +1,21 @@
-COLORS: dict[str, dict[str, str]] = {
+COLORS: dict[str, dict[str, dict[str, str]]] = {
         "deep blue": {
-            "bg_main": "#011127",
-            "bg_labelTime": "#062656",
-            "fg_labelTime": "#4FC500",
-            "bg_button": "#062656",
-            "fg_button": "#4FC500",
-            "bg_label": "#89EBEB",
-            "fg_label": "#000000",
-            "actbg_button": "#38418A",
-            "actfg_button": "#4FC500"
+            "main": {
+                "bg": "#011127",
+                },
+            "label_time": {
+                "bg": "#062656",
+                "fg": "#4FC500"
+                },
+            "button": {
+                "bg": "#062656",
+                "fg": "#4FC500",
+                },
+            "label": {
+                "bg": "#89EBEB",
+                "fg": "#000000",
+                "activebackground": "#38418A",
+                "activeforeground": "#4FC500"
+                }
         }
 }
